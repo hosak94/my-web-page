bd3eefb0277cec84798c5f6f91502f27d41680d6
hosak94 <yigitcanhosak@gmail.com>
2023-06-29 17:45:32 +0300

    projects mobile style edited

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Element } from "react-scroll";
 import { projectData } from "../data/projects.data";
 import "../styles/projects.css";
-import "../styles/projectsForMobile.css";
 import { useTranslation } from "react-i18next";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
@@ -38,9 +37,6 @@ export default function Projects() {
     projectData[(currentProjectIndex + 1) % projectData.length],
     projectData[(currentProjectIndex + 2) % projectData.length],
   ];
-  const displayedProjectsForMobile = [projectData[currentProjectIndex]];
-  //telefon formatı için displayedProjects 3 ten 1'e düşürüldü.
-  //projectsForMobile.css dosyasında telefon için css kodları düzenlendi.
 
   return (
     <Element name="projectsScroll" className="projects-container">
@@ -79,37 +75,7 @@ export default function Projects() {
             </div>
           ))}
         </div>
-        {/* Telefon için tekrar map kullandım. 
-        Sadece (min-width: 319px) and (max-width: 480px) aralığında görünür oluyor.
-        Not:Başka bir çözüm youlu araştır. */}
-        <div className="projects-for-mobile">
-          {displayedProjectsForMobile.map((project) => (
-            <div className="projects-info-mobile" key={project.projectName}>
-              <img
-                src={`${process.env.PUBLIC_URL}/project-photo/${project.projectImg}`}
-                alt={t(project.projectName)}
-                onClick={() => handleOnClick(project.projectUrl)}
-              />
-              <div className="project-description-mobile">
-                <h4>{t(project.projectName)}</h4>
-                <p>{t(project.projectDescription)}</p>
-              </div>
-              <div className="project-features-mobile">
-                {project.projectSkill.map((skill) => (
-                  <span key={skill}>{skill}</span>
-                ))}
-              </div>
-              <div className="project-link-mobile">
-                <span onClick={() => handleOnClick(project.projectUrl)}>
-                  Github
-                </span>
-                <span onClick={() => handleOnClick(project.projectUrl)}>
-                  View Site
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
+
         <div>
           <button className="next-button" onClick={handleNextProject}>
             <FaArrowRight />
--- src/data/projects.data.js
+++ src/data/projects.data.js
@@ -32,4 +32,37 @@ export const projectData = [
     projectUrl: "https://redux-films.vercel.app/",
     gitHub: "https://github.com/hosak94/fsweb-s10g3-redux-watchlist.git",
   },
+  {
+    projectName: "1",
+    projectImg: "Calculator.png",
+    projectDescription: "Calculator_info",
+    projectSkill: ["react", "redux", "Javascript", "useReducer"],
+    projectUrl: "https://hosak-calculator.vercel.app/",
+    gitHub: "https://github.com/hosak94/fsweb-s10g1-reducer-calculator",
+  },
+  {
+    projectName: "2",
+    projectImg: "Notes.png",
+    projectDescription: "Grateful_Diary_info",
+    projectSkill: ["react", "router", "redux", "localStorage"],
+    projectUrl: "https://fsweb-s10-challenge-sooty.vercel.app/",
+    gitHub: "https://github.com/hosak94/fsweb-s10-challenge",
+  },
+  {
+    projectName: "3",
+    projectImg: "favoritemovie.png",
+    projectDescription: "Movie_app_info",
+    projectSkill: ["react", "Router", "Axios", "Redux"],
+    projectUrl: "https://fsweb-s10g2-redux-filmler-lyart.vercel.app/movies",
+    gitHub: "https://github.com/hosak94/fsweb-s10g2-redux-filmler",
+  },
+  {
+    projectName: "4",
+    projectImg: "MovieList.png",
+    projectDescription:
+      "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+    projectSkill: ["react", "axios", "middleware"],
+    projectUrl: "https://redux-films.vercel.app/",
+    gitHub: "https://github.com/hosak94/fsweb-s10g3-redux-watchlist.git",
+  },
 ];
